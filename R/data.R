2219dab63a75930161f00788ae30b248cf0841c9
Galileu Kim <galileukim@worldbank.org>
2025-11-20 11:51:16 -0500

    Update acled analysis
    This commit updates the ACLED analysis and verifies that the checks pass.

--- R/data.R
+++ R/data.R
@@ -3,6 +3,7 @@
 #' @format A data frame with 2726 rows and 3 variables:
 #' \describe{
 #'   \item{\code{country}}{character The name of the country}
+#'   \item{\code{country_code}}{World Bank country code}
 #'   \item{\code{year}}{double The year of the demonstration events}
 #'   \item{\code{events}}{double The number of demonstration events}
 #'}
@@ -17,6 +18,7 @@
 #'   \item{\code{week}}{double. The week number corresponding to the aggregation period.}
 #'   \item{\code{region}}{character. The ACLED-defined region in which the event occurred.}
 #'   \item{\code{country}}{character. The country in which the event occurred.}
+#'   \item{\code{country_code}}{World Bank country code}
 #'   \item{\code{admin1}}{character. The first-level administrative division (e.g., province, state) where the event took place.}
 #'   \item{\code{event_type}}{character. The broad category of the event (e.g., Battles, Protests, Violence against civilians).}
 #'   \item{\code{sub_event_type}}{character. The more specific type of event within the event_type (e.g., Armed clash, Peaceful protest).}
@@ -31,41 +33,13 @@
 #' @details The Armed Conflict Location & Event Data Project (ACLED) is a comprehensive dataset of political violence, protest, and disorder events across the world. This dataset aggregates ACLED event data for Asia at the weekly level, including event types such as battles, protests, riots, and violence against civilians. Each record summarizes the number of events, fatalities, and estimated population exposure for a specific week, country, and administrative region. Geographic centroids are provided for spatial analysis. For more information, see the official ACLED codebook: https://acleddata.com/resources/codebooks/
 "acled_asia"
 
-#' Macro-level World Bank WDI indicators
-#'
-#' This dataset contains a selection of macroeconomic and labor market indicators
-#' for multiple countries and years. The data are primarily sourced from the World
-#' Bank World Development Indicators (WDI) via the Data360 API. The variables have
-#' been renamed for convenience, but the original WDI variable codes are provided
-#' for reference.
-#'
-#' @format A tibble with 11652 rows and 14 variables:
+#' @title World Bank Map 
+#' @description A shapefile containing all relevant country boundaries and disputed areas used by the World Bank.
+#' @format A data frame with 526 rows and 2 variables:
 #' \describe{
-#'   \item{country_code}{ISO3 country code (originally from Data360 API).}
-#'   \item{year}{Year of observation (character, originally from Data360 API).}
-#'   \item{gdp_lcu}{Gross Domestic Product (local currency units). Original WDI code: WB_WDI_NY_GDP_MKTP_CN.}
-#'   \item{pexpenditure_lcu}{Total government expenditure (local currency units). Original WDI code: WB_WDI_GC_XPN_TOTL_CN.}
-#'   \item{prevenue_lcu}{Total government revenue (local currency units). Original WDI code: WB_WDI_GC_REV_XGRT_CN.}
-#'   \item{taxrevenue_lcu}{Total government tax revenue (local currency units). Original WDI code: WB_WDI_GC_TAX_TOTL_CN.}
-#'   \item{emp_pop_rate}{Employment-to-population ratio (in percent). Original WDI code: WB_WDI_SL_EMP_TOTL_SP_NE_ZS.}
-#'   \item{labor_force_total}{Labor force, total. Original WDI code: WB_WDI_SL_TLF_TOTL_IN.}
-#'   \item{labor_force_advanced_edu}{Labor force with advanced education (percentage of total working-age population with advanced education). Original WDI code: WB_WDI_SL_TLF_ADVN_ZS.}
-#'   \item{tot_pop}{Total population. Original WDI code: WB_WDI_SP_POP_TOTL.}
-#'   \item{government_expenditure_gdp}{General government final consumption expenditure (percentage of GDP). Original WDI code: WB_WDI_NE_CON_GOVT_ZS.}
-#'   \item{salaried_rate}{Share of employed people who are salaried (in percent). Original WDI code: WB_WDI_SL_EMP_WORK_ZS.}
-#'   \item{cpi}{Consumer Price Index, total. Original WDI code: WB_WDI_FP_CPI_TOTL.}
-#'   \item{ppp}{Purchasing Power Parity (local currency units per international USD). Original WDI code: WB_WDI_PA_NUS_PRVT_PP.}
-#'   \item{fiscal_balance}{Overall Fiscal Balance, USD, percentage of GDP.}
-#'   \item{emp_pop}{The population of employed i.e. `emp_pop_rate` * `tot_pop`.}
-#'   \item{salaried_pop}{The population of salaried workers i.e. `salaried_rate` * `emp_pop`.}
-#' }
-#'
-#' @details
-#' This dataset was prepared to support wage bill diagnostics and other fiscal
-#' and labor market analysis. All numeric variables have been coerced to numeric
-#' type. Missing values may exist for certain countries and years.
-#'
-#' @source Data360 API, World Bank World Development Indicators (WDI)
-#' \url{https://data.worldbank.org/indicator}
-#'
-"wdi"
+#'   \item{\code{country_code}}{character World Bank country code}
+#'   \item{\code{geometry}}{list Geometry of the country boundaries}
+#'}
+#' @details This dataset includes both the official country boundaries and any disputed areas as recognized by the World Bank.
+#' @source https://datacatalogfiles.worldbank.org/ddh-published/0038272/5/DR0095369/World%20Bank%20Official%20Boundaries%20(GeoJSON)/World%20Bank%20Official%20Boundaries%20-%20Admin%200.geojson
+"wb_map"
--- analysis/03-demonstration.R
+++ analysis/03-demonstration.R
@@ -1,16 +1,18 @@
-
 # ---------------------------------------------------------------------------- #
 #                                    set-up                                    #
 # ---------------------------------------------------------------------------- #
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(sf)
+library(rmapshaper)
+library(geojsonio)
 library(here)
 
 devtools::load_all()
 theme_set(
     theme_few()
-) 
+)
 
 # ---------------------------------------------------------------------------- #
 #                                 global trends                                #
@@ -18,11 +20,16 @@ theme_set(
 acled_events <- acled |>
     inner_join(
         wdi,
-        by = c("country", "year")
+        by = c("country_code", "year")
     ) |>
-    filter(year >= 2010) 
-    
+    left_join(
+        countryclass,
+        by = c("country_code")
+    )
+
+# pooled
 acled_events |>
+    filter(!is.na(income_group)) |>
     group_by(year) |>
     summarize(
         events = sum(events, na.rm = TRUE)
@@ -35,3 +42,54 @@ acled_events |>
         x = "Year",
         y = "Number of Events"
     )
+
+ggsave(
+    here("analysis", "figs", "global_demonstration_trends.png")
+)
+
+# by income group
+acled_events |>
+    filter(!is.na(income_group)) |>
+    group_by(income_group, year) |>
+    summarize(
+        events = sum(events, na.rm = TRUE)
+    ) |>
+    ggplot(aes(x = year, y = events, color = income_group)) +
+    geom_line() +
+    geom_point() +
+    scale_color_brewer(
+        palette = "Set1",
+    ) +
+    labs(
+        title = "Global Demonstration Events Over Time, by Income Group",
+        x = "Year",
+        y = "Number of Events"
+    )
+
+ggsave(
+    here("analysis", "figs", "global_demonstration_trends_income.png")
+)
+
+# by region
+acled_events |>
+    filter(!is.na(region)) |>
+    group_by(region, year) |>
+    summarize(
+        events = sum(events, na.rm = TRUE)
+    ) |>
+    ggplot(aes(x = year, y = events, color = region)) +
+    geom_line() +
+    geom_point() +
+    scale_color_brewer(
+        palette = "Set1",
+    ) +
+    labs(
+        title = "Global Demonstration Events Over Time, by Region",
+        x = "Year",
+        y = "Number of Events"
+    )
+
+ggsave(
+    here("analysis", "figs", "global_demonstration_trends_region.png")
+)
+
