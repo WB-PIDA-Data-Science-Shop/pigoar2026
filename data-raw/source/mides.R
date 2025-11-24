0934a54d7ba7bd19f7e376aa8c303a520923f524
Galileu Kim <galileukim@worldbank.org>
2025-11-24 16:11:55 -0500

    add new datasets and document them

--- data-raw/source/mides.R
+++ data-raw/source/mides.R
@@ -1,6 +1,6 @@
 ## code to prepare `mides` dataset goes here
 # available in: https://reproducibility.worldbank.org/index.php/catalog/72
-# access date: 7/3/2025
+# access date: 11/24/2025
 library(readr)
 library(dplyr)
 library(sf)
@@ -12,19 +12,19 @@ mides_input <- read_csv(
 )
 
 # download shape files
-brazil_mun_shp <- get_shapefile_from_zip_url(
+brazil_mun_shp <- get_shapefile(
   "https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2024/Brasil/BR_Municipios_2024.zip"
   ) |>
   st_read() |>
   st_simplify(dTolerance = 100)
 
-brazil_mun_shp <- brazil_mun_shp %>%
+brazil_mun_shp <- brazil_mun_shp |> 
   clean_names() |>
   rename(
     municipality_code = cd_mun
   )
 
-mides <- mides_input %>%
+mides <- mides_input |> 
   select(
     state,
     year,
