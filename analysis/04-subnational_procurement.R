c816b8bb696d5c4250216e4fc98aff63a27b0047
Galileu Kim <galileukim@worldbank.org>
2025-12-08 13:52:37 -0500

    novel estimation

--- analysis/04-subnational_procurement.R
+++ analysis/04-subnational_procurement.R
@@ -16,6 +16,11 @@ subnational_hr <- pigoar2026::rais_mun |>
   rename(
     municipality_code = id_municipio,
     year = ano
+  ) |> 
+  # weigh local governments by headcount, changing weights each year
+  group_by(year) |> 
+  mutate(
+    weighted_share_dismissed = (total_headcount/sum(total_headcount)) * share_dismissed
   )
 
 subnational_gov <- pigoar2026::mides |> 
@@ -27,6 +32,12 @@ subnational_gov <- pigoar2026::mides |>
   left_join(
     subnational_hr,
     by = c("municipality_code", "year")
+  ) |> 
+  mutate(
+    across(
+      is.numeric, 
+      \(col) scale(col)
+    )
   )
 
 # visualize --------------------------------------------------------------
@@ -36,11 +47,12 @@ subnational_gov |>
   ) |> 
   ggplot(
     aes(
-      x = share_dismissed,
-      y = weighted_average_delay,
-      color = gdp_per_capita_quartile
+      x = weighted_share_dismissed,
+      y = weighted_average_delay
+      # color = gdp_per_capita_quartile
     )
   ) +
+  # geom_point() +
   # plot binned averages
   stat_summary_bin(
     fun = "mean",
@@ -49,11 +61,8 @@ subnational_gov |>
     size = 3
   ) +
   geom_smooth(method = "lm") +
-  scale_y_log10() +
-  facet_wrap(
-    vars(gdp_per_capita_quartile),
-    nrow = 2
-  )
+  scale_x_log10() +
+  scale_y_log10()
 
 ggsave(
   here("analysis", "figs", "correlation", "subnat_procur_hrm.png"),
@@ -61,41 +70,60 @@ ggsave(
 )
 
 # regression -------------------------------------------------------------
-lm(
-  weighted_average_delay ~ share_dismissed + log(gdp_per_capita) +
-    idhm + log(population) +
-    as.factor(state) + as.factor(year),
+baseline_lm <- lm(
+  weighted_average_delay ~ weighted_share_dismissed,
   data = subnational_gov
+)
+
+controls_lm <- update(baseline_lm, . ~ . + gdp_per_capita + idhm + population)
+fe_lm <- update(controls_lm, . ~ . + as.factor(state) + as.factor(year))
+
+list(
+  "Baseline" = baseline_lm,
+  "Controls" = controls_lm,
+  "Fixed-effects" = fe_lm
 ) |> 
-  tidy() |> 
+  purrr::map_dfr(tidy, .id = "model") |> 
   filter(
     !grepl("Intercept|as.factor", term)
   ) |> 
-  dwplot() %>%
+  dwplot(
+    dot_args = list(
+      aes(colour = model, shape = model),
+      size = 5
+    ),
+    whisker_args = list(size = 1.5)
+  )  |> 
   relabel_predictors(
-    c(share_dismissed = "Dismissal Rate", weighted_average_delay = "Weighted Procurement Delay")
-  )+
-  theme_bw() + xlab("Coefficient") + ylab("") +
-  geom_vline(
-    xintercept = 0, colour = "grey60", linetype = 2
-  )
-  
-subnational_gov |> 
-  filter(
-    !is.na(gdp_per_capita_tercile)
-  ) |> 
-  ggplot(
-    aes(
-      share_dismissed,
-      weighted_average_delay,
-      color = gdp_per_capita_tercile
+    c(
+      weighted_share_dismissed = "Dismissal Rate", 
+      weighted_average_delay = "Weighted Procurement Delay",
+      gdp_per_capita = "GDP per capita",
+      idhm = "Human Development Index",
+      population = "Population"
     )
   ) +
-  geom_point(
-    alpha = 0.5
+  xlab("Coefficient") + 
+  ylab("") +
+  geom_vline(
+    xintercept = 0, colour = "grey60", linetype = 2
   ) +
-  geom_smooth(
-    method = "lm"
-  ) +
-  scale_x_log10() +
-  scale_y_log10()
+  theme_bw() + 
+  labs(x = "Coefficient Estimate with 95% CIs", y = "") +
+  theme(
+        legend.position = "bottom"
+  ) + 
+  scale_shape_discrete(
+    name = "Models", 
+    breaks = c(0, 1)
+  ) + 
+  scale_colour_grey(
+    start = .3, end = .7, name = "Models"
+  ) 
+
+  ggsave(
+    here("analysis", "figs", "procurement", "subnat_procur_hrm_fit.png"),
+    height = 6,
+    width = 9,
+    bg = "white"
+  )
